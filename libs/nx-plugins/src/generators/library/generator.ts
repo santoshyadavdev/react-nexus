a945bfb3ece7b8e13917095bdf2a679259f074d5
Santosh Yadav <santosh.yadav198613@gmail.com>
2024-06-16 18:21:41 +0200

    work on generator

--- libs/nx-plugins/src/generators/library/generator.ts
+++ libs/nx-plugins/src/generators/library/generator.ts
@@ -1,39 +1,36 @@
 import {
-  addProjectConfiguration,
   formatFiles,
-  generateFiles,
+  readProjectConfiguration,
   Tree,
 } from '@nx/devkit';
 import { Linter } from '@nx/eslint';
 import {
-  componentTestGenerator,
   libraryGenerator as ReactLibraryGenerator,
 } from '@nx/react';
 
 import { LibraryGeneratorSchema } from './schema';
-import { CreateComponentSpecFileSchema } from '@nx/react/src/generators/component-cypress-spec/component-cypress-spec';
 
 export async function libraryGenerator(
   tree: Tree,
   options: LibraryGeneratorSchema
 ) {
-  // const projectRoot = `libs/${options.name}`;
+  try {
+    await ReactLibraryGenerator(tree, {
+      name: options.name,
+      linter: Linter.EsLint,
+      style: 'scss',
+      component: true,
+    });
 
-  await ReactLibraryGenerator(tree, {
-    name: options.name,
-    linter: Linter.EsLint,
-    style: 'scss',
-  });
+    const project = readProjectConfiguration(tree, options.name);
 
-  const cypressSchema: CreateComponentSpecFileSchema = {
-    project: options.name,
-    componentPath: '',
-  };
+    console.log(project);
 
-  await componentTestGenerator(tree, cypressSchema);
-
-  // generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
-  await formatFiles(tree);
+    // generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+    await formatFiles(tree);
+  } catch (err) {
+    console.log(err);
+  } // const projectRoot = `libs/${options.name}`;
 }
 
 export default libraryGenerator;
